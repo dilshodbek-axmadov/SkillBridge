7ddb95d3768466477b27f1e1807afe2f57ea2761
dilshod <wiut00016668@gmail.com>
2026-04-27 08:01:16 +0500

    Changed in Dockerfile

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+echo "[entrypoint] Collecting static files..."
+python manage.py collectstatic --noinput
+
 echo "[entrypoint] Running database migrations..."
 python manage.py migrate --noinput
 
--- backend/skillbridge/settings.py
+++ backend/skillbridge/settings.py
@@ -233,10 +233,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = '/app/staticfiles'  # ← Add this line
 
+# Media files (user uploads)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/app/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
